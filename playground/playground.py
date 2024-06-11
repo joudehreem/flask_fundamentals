96b53fa6c13e366768152ec227a2cf7bb55a1651
Reem Joudeh <joudehreem@gmail.com>
2024-06-11 11:51:43 +0300

    final

---
+++ playground/playground.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+app=Flask(__name__)
+
+@app.route('/play')
+@app.route('/play/<int:x>')
+@app.route('/play/<int:x>/<color>')
+def playground(x, color):
+  return render_template('playground.html',header='Welcome!',box=x,color=color,title='PlayGround')
+
+if __name__=="__main__":
+    app.run(debug=True)
+
+
+# @app.route('/')
+# @app.route('/play')
+# @app.route('/play/<int:x>')
+# @app.route('/play/<int:x>/<color>')
+# def playground(x=3, color='blue'):
+#     return render_template('playground.html', header='Welcome!', boxes=x, color=color, title='PlayGround')
+
+
+
